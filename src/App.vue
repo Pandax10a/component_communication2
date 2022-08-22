a727f58fe8a11314efd9c25211d418775340c115
panda1 <blueblue0011@gmail.com>
2022-08-22 16:37:39 -0700

    made changes to the prop value passing back to component helloworld

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="pass this on"/>
   </div>
 </template>
 
---
+++ src/components/SmallUserProfile.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <button>click me</button>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'small-user-profile'
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
